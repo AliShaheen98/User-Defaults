e95c03fd926cf6f6b18fa5299487e3fc5bc1c3ae
Ali <as598102@icloud.com>
2020-11-26 00:49:34 +0200

    first commit

--- User Defaults/ViewController.swift
+++ User Defaults/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true))
+
+
+        muteSwitch.isOn = defaults.bool(forKey: "muteSwitch")
+        print(defaults.bool(forKey: "muteSwitch"))
+
+        soungSlider.value = defaults.float(forKey: "soungSlider")
+        print(defaults.float(forKey: "soungSlider")
+)
+        speakerSegmented.selectedSegmentIndex = defaults.integer(forKey: "speakerSegmented")
+        print(defaults.integer(forKey: "speakerSegmented")
+)
+        
+        modeSegmented.selectedSegmentIndex = defaults.integer(forKey: "modeSegmented")
+        print(defaults.integer(forKey: "modeSegmented")
+)
+        
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var muteSwitch: UISwitch!
+    
+    @IBOutlet weak var soungSlider: UISlider!
+    
+    @IBOutlet weak var speakerSegmented: UISegmentedControl!
+    
+    @IBOutlet weak var modeSegmented: UISegmentedControl!
+    
+    
+    @IBAction func modeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
 
+        }else{
+            view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
 
-}
+        }
+    }
+    
+    
+    @IBAction func SaveButton(_ sender: UIButton) {
+
+        defaults.set(muteSwitch.isOn,forKey: "muteSwitch")
+        defaults.set(soungSlider.value, forKey: "soungSlider")
+        defaults.set(speakerSegmented.selectedSegmentIndex, forKey: "speakerSegmented")
+        defaults.set(modeSegmented.selectedSegmentIndex, forKey: "modeSegmented")
+        
+    }
+    
+     }
 
